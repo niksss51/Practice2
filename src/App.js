b131f46642f56f7745b472041be9dc525b50a225
niksss51 <140971079+niksss51@users.noreply.github.com>
2023-07-30 22:39:08 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title="ResumePro" />
+      {/* <Navbar title={4} /> */}
+
+    </>
+  );
+}
+
+export default App;
